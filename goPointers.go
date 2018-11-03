3ea6ce4fb02bb17e54cb69e2f72c43cd7a7e1313
Harshit Raj <raj.0489@gmail.com>
2018-11-02 20:32:05 -0400

    basics and pointer

---
+++ goPointers.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 15
+	a := &x //memory address
+	fmt.Println(a)
+	fmt.Println(*a)
+}
--- gotut.go
+++ gotut.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func add(x, y float64) float64 {
+	return x + y
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	num1, num2 := 5.6, 9.5
+	fmt.Println(add(num1, num2))
+	w1, w2 := "Hey", "There"
+	fmt.Println(multiple(w1, w2))
+}
+
+func multiple(a, b string) (string, string) {
+	return a, b
 }
